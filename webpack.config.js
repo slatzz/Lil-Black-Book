fcf9fe3017fbad7f4735690932809c1e05480b2a
Kris <kris@Lauras-MacBook-Pro-2.local>
2020-09-21 15:19:34 -0500

    Set up initial webpack config!!!!!!!! YEET

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 const path = require('path')
+const nodeExternals = require('webpack-node-externals');
 
 const config = {
-  entry: [path.resolve(__dirname, 'src/index.js')],
-  output: {
-    path: path.resolve(__dirname, '/build'),
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  mode: 'development',
-  resolve: {
+    entry: [path.resolve(__dirname, 'src/index.js')],
+    target: 'node',
+    externals: [nodeExternals()],
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    mode: 'production',
+    resolve: {
     modules: [path.resolve('node_modules'), 'node_modules']
   },
   module: {
