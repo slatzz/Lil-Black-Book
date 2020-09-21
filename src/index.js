cbe5e18425f8630c9488b50759918bf5776c9734
Kris <kris@Lauras-MacBook-Pro-2.local>
2020-09-21 16:11:09 -0500

    installed body-parser, server running, we gucci

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require ('dotenv').config()
 const express = require('express')
 const app = express();
+const bodyparser = require('body-parser')
 const { PORT } = process.env;
 
 app.get('/', (req, res) => {
@@ -8,6 +9,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(PORT, () => {
-
 	console.info(`Dis _ooSPOOKYoo_ app is listening on ${PORT}`)
 })
