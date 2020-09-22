90e05689aa9bc1fcd7ccbb79495ef6567ba13829
Kris <kris@Lauras-MacBook-Pro-2.local>
2020-09-22 00:42:03 -0500

    Didn't change much, created server folder, working on CRUD

---
+++ server/index.js
@@ -0,0 +1,61 @@
+// import ReactDOM from 'react-dom';
+
+require ('dotenv').config();
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+const mysql = require('mysql');
+const { PORT } = process.env;
+// const React = require('./components/App.js')
+// const index = require('index.html');
+const bodyParser = require('body-parser');
+
+app.use(express.static('root'));
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
+
+////// MYSQL DATABASE CONNECTION //////
+const DATABASE = 'spooky';
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: DATABASE
+});
+
+// connection.connect( err => {
+//     if(err){
+//         console.log(err, 'Cannot find your spooky boys!')
+//     } else {
+//         console.log('Happy Ghost Hunting!')
+//     }
+// })
+const mockResponse = {
+    'name': 'Spooky Boys', 
+    'specialty':'Poltergeists'
+};
+
+connection.connect( err => {
+    if(err){
+        console.log(err, 'Cannot find your spooky boys!')
+    } else {
+        console.log('Happy Ghost Hunting!')
+    }
+})
+////////////////////////////////////////////////
+app.get('/', (req, res) => {
+    res.status(200).send(mockResponse);
+})
+
+app.post('/', (req, res) => {
+    res.status(201).send({"name": "Ghost Bros", "specialty": "Daemons"});
+})
+
+app.listen(PORT, () => {
+	console.info(`Dis _ooSPOOKYoo_ app is listening on ${PORT}`)
+})
+
+
+
---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+// // import React from 'react';
+
+
+// class App extends React.Component {
+
+//     render() {
+//       return (
+//         <div>
+//           <h1>Hello, world!</h1>
+//           <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+//         </div>
+//       );
+//     }
+//   }
+
+
+//   ReactDOM.render(
+//     App, document.getElementById('app')
+//   );
+
+// window.App = App;
+
+
+
+// import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+
+// class App extends Component {
+//   render() {
+//    return <h1>Hello World!</h1>
+//   }
+// }
+
+// ReactDOM.render(<App />, document.getElementById('app'));
+// module.exports.App = App;
