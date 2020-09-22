20487fe9337cb071ad65858362070359f3d3c8d9
Kris <kris@Lauras-MacBook-Pro-2.local>
2020-09-22 13:18:08 -0500

    Debugged db issue - submitting queries now beeeeee

---
+++ server/db.js
@@ -0,0 +1,39 @@
+const Sequelize = require('sequelize');
+const mysql = require('mysql');
+
+////// MYSQL DATABASE CONNECTION //////
+const DB_NAME = 'spooky';
+const DB_USER = 'root';
+const DB_PASS = '';
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+    host: 'localhost',
+    dialect: 'mysql',
+    timestamps: false
+  });
+
+sequelize.authenticate()
+  .then( () => {
+      console.log('Happy Ghost Hunting!')
+  })
+  .catch( err => {
+    console.log(err, 'Cannot find your spooky boys!')
+  })
+
+// const DATABASE = 'spooky';
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: DATABASE
+// });
+
+// connection.connect( err => {
+//     if(err){
+//         console.log(err, 'Cannot find your spooky boys!')
+//     } else {
+//         console.log('Happy Ghost Hunting!')
+//     }
+// })
+
+module.exports = sequelize;
--- server/index.js
+++ server/index.js
@@ -1,49 +1,58 @@
+
+////////////////////////////////////////////////////////////////////////////////////
+//////////////// SET UP ////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
 require ('dotenv').config();
+const sequelize = require('./db')
 const express = require('express');
 const path = require('path'); 
+const { Sequelize } = require('sequelize');
 const app = express();
-const mysql = require('mysql');
 const { PORT } = process.env;
 const DIR = path.join(__dirname, '../build');
 const HTML_FILE = path.join(DIR, 'index.html');
+app.use(express.static(DIR));
 // const bodyParser = require('body-parser');
 // app.use(bodyParser.urlencoded({extended:true}));
 // app.use(bodyParser.json());
-app.use(express.static(DIR));
 
-////// MYSQL DATABASE CONNECTION //////
-const DATABASE = 'spooky';
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: DATABASE
-});
 
+// -------- TEST --------- //
 const mockResponse = {
     'name': 'Spooky Boys', 
     'specialty':'Poltergeists'
 };
 
-connection.connect( err => {
-    if(err){
-        console.log(err, 'Cannot find your spooky boys!')
-    } else {
-        console.log('Happy Ghost Hunting!')
-    }
-})
-////////////////////////////////////////////////
+const Query = sequelize.define('Query', {
+    id: { 
+        type: Sequelize.NUMBER, 
+        primaryKey: true,
+        auto_increment: true
+    },
+    value: Sequelize.STRING,
+}, { timestamps: false })
+////////////////////////////////////////////////////////////////////////////////////
+//////////////// CRUD //////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
 app.get('/api', (req, res) => {
-    res.send(mockResponse);
+    Query.create({ value: 'Pop a 911'})
+        .then( (query) => {
+            console.log(JSON.stringify(query))
+        })
 })
 
 app.get('/', (req, res) => {
     res.sendFile(HTML_FILE);
 })
 
-// app.post('/', (req, res) => {
-//     res.status(201).send({"name": "Ghost Bros", "specialty": "Daemons"});
-// })
+app.post('/', (req, res) => {
+    res.status(201).send({"name": "Ghost Bros", "specialty": "Daemons"});
+})
+
+
+////////////////////////////////////////////////////////////////////////////////////
+//////////////// PORT //////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
 
 app.listen(PORT, () => {
 	console.info(`Dis _ooSPOOKYoo_ app is listening on ${PORT}`)
